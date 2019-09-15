39ac9dca5d0356fccd9275269b7a5da789c56c2e
Nico <nicolas1.schmitt@epitech.eu>
2019-09-14 17:55:45 -0700

    feat(component): Input component

--- components/Input.js
+++ components/Input.js
@@ -1,111 +1,45 @@
-import {Input as AntInput} from 'antd';
 import React from "react";
+import {Input as AntInput} from 'antd';
 
-const {Search} = AntInput;
-
-class Input extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-    }
+export default class Input extends React.Component {
+    static defaultProps = {
+        addonAfter: "", // (string) The label text displayed after (on the right side of) the input field
+        addonBefore: "", // (string) The label text displayed before (on the left side of) the input field
+        allowClear: false, // (boolean) allow to remove input content with clear icon
+        defaultValue: "", // (string) default value of input
+        disabled: false, // (boolean) Whether the input is disabled
+        enterButton: false, // (boolean) to show an enter button after input. This prop is conflict with addon | work only if this.props.type is "search"
+        id: "", // (string) the id of the input
+        type: "default" | "search" | "password", // (string) type of the input
+        onChange: undefined, // (function(e)) callback when user input
+        onPressEnter: undefined, // (function(e)) The callback function that is triggered when Enter key is pressed
+        onSearch: undefined, // (function(value, event)) The callback function that is triggered when you click on the search-icon or press Enter key
+        // only work is this.props.type is "search" | will be overwritten by this.props.onPressEnter if both are in props
+        placeholder: "", // (string) placeholder of the input
+        prefix: "", // (string) the prefix icon of the input
+        size: "default", // (string) size of the input
+        suffix: "" // (string) the suffix icon of the input | if this.props.type is "password", suffix icon will be replaced by the icon "hide" for password
+    };
 
     render() {
-        if (this.props.isSearch) {
-            return (
-                <div style={this.props.style}>
-                    <Search
-                        addonAfter={this.props.addonAfter}
-                        addonBefore={this.props.addonBefore}
-                        allowClear={this.props.allowClear}
-                        defaultValue={this.props.defaultValue}
-                        disabled={this.props.disabled}
-                        enterButton={this.props.enterButton}
-                        id={this.props.id}
-                        onChange={this.props.onChange}
-                        onSearch={this.props.onSearch}
-                        placeholder={this.props.placeholder}
-                        prefix={this.props.prefix}
-                        suffix={this.props.suffix}
-                    />
-                </div>
-            )
-        } else if (this.props.isPassword) {
-            return (
-                <div style={this.props.style}>
-                    <AntInput.Password
-                        addonAfter={this.props.addonAfter}
-                        addonBefore={this.props.addonBefore}
-                        allowClear={this.props.allowClear}
-                        defaultValue={this.props.defaultValue}
-                        disabled={this.props.disabled}
-                        id={this.props.id}
-                        onChange={this.props.onChange}
-                        onPressEnter={this.props.onPressEnter}
-                        placeholder={this.props.placeholder}
-                        prefix={this.props.prefix}
-                        size={this.props.size}
-                        suffix={this.props.suffix}
-                    />
-                </div>
-            )
-        } else {
-            return (
-                <div style={this.props.style}>
-                    <AntInput
-                        addonAfter={this.props.addonAfter}
-                        addonBefore={this.props.addonBefore}
-                        allowClear={this.props.allowClear}
-                        defaultValue={this.props.defaultValue}
-                        disabled={this.props.disabled}
-                        id={this.props.id}
-                        onChange={this.props.onChange}
-                        onPressEnter={this.props.onPressEnter}
-                        placeholder={this.props.placeholder}
-                        prefix={this.props.prefix}
-                        size={this.props.size}
-                        suffix={this.props.suffix}
-                    />
-                </div>
-            )
-        }
+        console.log(this.props);
 
+        let inputType;
+        const {enterButton, onSearch, ...props} = this.props;
+
+        switch (this.props.type) {
+            case "default":
+                inputType = <AntInput {...props}/>;
+                break;
+            case "search":
+                inputType = <AntInput.Search {...props} enterButton={enterButton} onSearch={onSearch}/>;
+                break;
+            case "password":
+                inputType = <AntInput.Password {...props}/>;
+                break;
+            default:
+                inputType = <AntInput/>
+        }
+        return inputType
     }
 }
-
-function onChangeDefault(e) {
-    console.log('onChangeDefault');
-    // console.log("e", e.target.value);
-}
-
-function onPressEnterDefault(e) {
-    console.log('onPressEnterDefault');
-    // console.log('value', e.target.value);
-}
-
-function onSearchDefault(value, event) {
-    console.log('onSearchDefault');
-    // console.log('value', value);
-    // console.log('event', event);
-}
-
-Input.defaultProps = {
-    addonAfter: null, // (string) The label text displayed after (on the right side of) the input field
-    addonBefore: null, // (string) The label text displayed before (on the left side of) the input field
-    allowClear: false, // (boolean) allow to remove input content with clear icon
-    defaultValue: null, // (string) default value of input
-    disabled: false, // (boolean) Whether the input is disabled
-    enterButton: false, // (boolean) to show an enter button after input. This prop is conflict with addon | work only if isSearch is "true"
-    id: null, // (string) the id of the input
-    isPassword: false, // (boolean) display icon to hide/show the password | if this.props.isSearch is "true", isPassword will be ignored
-    isSearch: false, // (boolean) if there is the icon for search
-    onChange: onChangeDefault, // (function(e)) callback when user input
-    onPressEnter: onPressEnterDefault, // (function(e)) The callback function that is triggered when Enter key is pressed
-    onSearch: onSearchDefault, // (function(value, event)) The callback function that is triggered when you click on the search-icon or press Enter key
-                               // only work is this.props.isSearch is "true" | will be overwritten by this.props.onPressEnter if both are in props
-    placeholder: null, // (string) placeholder of the input
-    prefix: null, // (string) the prefix icon of the input
-    size: 'default', // (string) size of the input
-    suffix: null // (string) the suffix icon of the input | if this.props.isPassword is "true", suffix icon will be replaced by the icon "hide" for password
-};
-
-export default Input;
