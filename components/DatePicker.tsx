6dc4d0974d6bfe28e77b4ae3b48125cdbc1bdf9a
heim_v <victor.heim@epitech.eu>
2019-09-15 20:10:29 +0200

    feat (config): adding typescript config feat (component): adding Button component feat (component): adding the beginning of the DatePicker component

---
+++ components/DatePicker.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {DatePicker as AntDatepicker} from 'antd'
+
+declare type Props = {}
+
+declare type State = {}
+
+export default class DatePicker extends React.Component<Props, State> {
+    render() {
+        return <AntDatepicker />
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,14 @@
-import React from "react";
-import Home from "./Home"
+import React from 'react'
+import Head from 'next/head'
+import Button from "../components/Button";
 
-export default function Start() {
-    return (
-        <Home/>
-    );
-}
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+      <Button text={"bite"} color={"primary"} >TEST</Button>
+  </div>
+);
 
-// export default function Blog() {
-//     let list = todoList();
-//
-//     function changeColor(id, done) {
-//         list.map(task => {
-//             if (task.id === id) {
-//                 task.done = done;
-//                 console.log('done', task);
-//             }
-//         });
-//         console.log('task', list);
-//         return list;
-//     }
-//
-//     return (
-//         <Layout>
-//             <h1>My TODOList</h1>
-//             <ul>
-//                 {list.map(tasktt => (
-//                     <Task key={tasktt.id} task={tasktt} change={changeColor}/>
-//                 ))
-//                 }
-//             </ul>
-//         </Layout>
-//     );
-// }
+export default Home
